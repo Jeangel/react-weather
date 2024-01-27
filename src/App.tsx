590c5d04114a5242860f3bb293ea4952520fd128
John Angel <usw4763@gmail.com>
2024-01-27 12:44:11 +0100

    WIP mobile styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import styles from "./app.module.css";
+import CurrentWeather from "./components/organisms/CurrentWeather";
+import WeatherDashboard from "./components/organisms/WeatherDashboard";
+import { SearchProvider } from "./context/SearchContext";
+
 function App() {
-  return <p>Hello</p>;
+  return (
+    <SearchProvider>
+      <div className={styles.root}>
+        <CurrentWeather />
+        <WeatherDashboard />
+      </div>
+    </SearchProvider>
+  );
 }
 
 export default App;
---
+++ src/components/organisms/WeatherDashboard/index.tsx
@@ -0,0 +1,7 @@
+import styles from "./styles.module.css";
+
+const WeatherDashboard = () => {
+  return <div className={styles.root}></div>;
+};
+
+export default WeatherDashboard;
