d44d652110bca4298e53edd977e9ae02bc5b0f1d
John Angel <usw4763@gmail.com>
2024-01-27 12:41:38 +0100

    Connect Search component with app state

--- src/components/molecules/Search/index.tsx
+++ src/components/molecules/Search/index.tsx
@@ -3,21 +3,28 @@ import styles from "./styles.module.css";
 import Button from "@/components/atoms/Button";
 import { IconSearch } from "@tabler/icons-react";
 import { useState } from "react";
+import useSearch from "@/hooks/useSearch";
 
 const Search = () => {
   const [value, setValue] = useState("");
+  const { setQuery } = useSearch();
+
+  const handleOnSearch = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setQuery(value);
+  };
 
   return (
-    <div className={styles.root}>
+    <form className={styles.root} onSubmit={handleOnSearch}>
       <Input
         placeholder="Search new place"
         className={styles.input}
         onChange={(e) => setValue(e.target.value)}
       />
-      <Button disabled={!value}>
+      <Button disabled={!value} aria-label="Search" type="submit">
         <IconSearch stroke={1} size={20} />
       </Button>
-    </div>
+    </form>
   );
 };
 
