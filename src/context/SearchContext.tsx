6b92f3399dffafed6507fd071ace84a08e50899f
John Angel <usw4763@gmail.com>
2024-01-27 12:40:58 +0100

    Add initial context to handle app state

---
+++ src/context/SearchContext.tsx
@@ -0,0 +1,47 @@
+import useWeather, { UseWeatherResponse } from "@/hooks/useWeather";
+import { createContext, useEffect, useState } from "react";
+
+interface SearchContextValue {
+  weather?: UseWeatherResponse;
+  location?: object;
+  isLoading: boolean;
+  query: string;
+  setQuery: (query: string) => void;
+}
+
+const SearchContext = createContext<SearchContextValue>({
+  weather: undefined,
+  location: undefined,
+  isLoading: false,
+  query: "",
+  setQuery: () => {},
+});
+
+interface SearchProviderProps {
+  children: React.ReactNode;
+}
+
+export const SearchProvider = ({ children }: SearchProviderProps) => {
+  const [query, setQuery] = useState("");
+  const {
+    data: weather,
+    refetch: fetchWeather,
+    isLoading,
+  } = useWeather({ city: query });
+
+  useEffect(() => {
+    if (query) {
+      fetchWeather();
+    }
+  }, [query, fetchWeather]);
+
+  return (
+    <SearchContext.Provider
+      value={{ query, setQuery, isLoading, location: undefined, weather }}
+    >
+      {children}
+    </SearchContext.Provider>
+  );
+};
+
+export default SearchContext;
